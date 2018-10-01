c03d1fd9b29e6a93a3918ca92de46798390e1bfa
Christopher Coleman <chris.d.coleman.1995@gmail.com>
2018-10-01 12:35:01 -0400

    Core Files
    These are the core files for the website to work.

---
+++ ButtonActions.js
@@ -0,0 +1,71 @@
+function openGen1Html() {
+    window.open("C:/Users/Chris/Documents/Projects/TypeChecker(v.0.0.3)/gen1typechecker.html", "_self");
+}
+
+function openGen2_5Html() {
+    window.open("C:/Users/Chris/Documents/Projects/TypeChecker(v.0.0.3)/gen2_5typechecker.html", "_self");
+}
+
+function openResourcesHtml() {
+    window.open("C:/Users/Chris/Documents/Projects/TypeChecker(v.0.0.3)/resources.html", "_self");
+}
+
+function openGen6OnHtml() {
+    window.open("C:/Users/Chris/Documents/Projects/TypeChecker(v.0.0.3)/gen6typechecker.html", "_self");
+}
+
+function openWalkthroughRedAndBlue() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Red_and_Blue_walkthrough");
+}
+
+function openWalkthroughYellow() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Yellow_walkthrough");
+}
+
+function openWalkthroughGoldAndSilver() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Gold_and_Silver_walkthrough");
+}
+
+function openWalkthroughCrystal() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Crystal_walkthrough");
+}
+
+function openWalkthroughRubyAndSapphire() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Ruby_and_Sapphire_walkthrough");
+}
+
+function openWalkthroughFireRedAndLeafGreen() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:FireRed_and_LeafGreen_walkthrough");
+}
+
+function openWalkthroughEmerald() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Emerald_walkthrough");
+}
+
+function openWalkthroughDiamondAndPearl() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Diamond_and_Pearl_walkthrough");
+}
+
+function openWalkthroughPlatinum() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Platinum_walkthrough");
+}
+
+function openWalkthroughHGAndSS() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:HeartGold_and_SoulSilver_walkthrough");
+}
+
+function openWalkthroughBlackAndWhite() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Black_and_White_walkthrough");
+}
+
+function openWalkthroughBlack2AndWhite2() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Black_2_and_White_2_walkthrough");
+}
+
+function openWalkthroughXAndY() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:X_and_Y_walkthrough");
+}
+
+function openWalkthroughORAndAS() {
+    window.open("http://bulbapedia.bulbagarden.net/wiki/Appendix:Omega_Ruby_and_Alpha_Sapphire_walkthrough");
+}
