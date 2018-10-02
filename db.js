1baf12693349a63bfc8e6639fa993b7f6e74fc89
Christopher Coleman <chris.d.coleman.1995@gmail.com>
2018-10-02 14:00:37 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -2,76 +2,149 @@ db  = {
     "Attack": [
         {"key":"Normal", "values": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0.5", "0", "1"]},
         {"key":"Fire", "values": ["1", "0.5", "0.5", "1", "2", "2", "1", "1", "1", "1", "1", "2", "0.5", "1", "0.5"]},
-        {"key":"none", "Water": ["1", "2", "0.5", "1", "0.5", "1", "1", "1", "2", "1", "1", "1", "2", "1", "0.5"]},
-        {"key":"none","Electric": ["1", "1", "2", "0.5", "0.5", "1", "1", "1", "0", "2", "1", "1", "1", "1", "0.5"]},
-        {"key":"none","Grass": ["1", "0.5", "2", "1", "0.5", "1", "1", "0.5", "2", "0.5", "1", "0.5", "2", "1", "0.5"]},
-        {"key":"none","Ice": ["1", "1", "0.5", "1", "2", "0.5", "1", "1", "2", "2", "1", "1", "1", "1", "2"]},
-        {"key":"none","Fighting": ["2", "1", "1", "1", "1", "2", "1", "0.5", "1", "0.5", "0.5", "0.5", "2", "0", "1"]},
-        {"key":"none","Poison": ["1", "1", "1", "1", "2", "1", "1", "0.5", "0.5", "1", "1", "2", "0.5", "0.5", "1"]},
+        {"key":"Water", "values": ["1", "2", "0.5", "1", "0.5", "1", "1", "1", "2", "1", "1", "1", "2", "1", "0.5"]},
+        {"key":"Electric","values": ["1", "1", "2", "0.5", "0.5", "1", "1", "1", "0", "2", "1", "1", "1", "1", "0.5"]},
+        {"key":"Grass","values": ["1", "0.5", "2", "1", "0.5", "1", "1", "0.5", "2", "0.5", "1", "0.5", "2", "1", "0.5"]},
+        {"key":"Ice","values": ["1", "1", "0.5", "1", "2", "0.5", "1", "1", "2", "2", "1", "1", "1", "1", "2"]},
+        {"key":"Fighting","values": ["2", "1", "1", "1", "1", "2", "1", "0.5", "1", "0.5", "0.5", "0.5", "2", "0", "1"]},
+        {"key":"Poison","values": ["1", "1", "1", "1", "2", "1", "1", "0.5", "0.5", "1", "1", "2", "0.5", "0.5", "1"]},
         {"key":"Ground", "values": ["1", "2", "1", "2", "0.5", "1", "1", "2", "1", "0", "1", "0.5", "2", "1", "1"]},
-        {"key":"none","Flying": ["1", "1", "1", "0.5", "2", "1", "2", "1", "1", "1", "1", "2", "0.5", "1", "1"]},
-        {"key":"none","Psychic": ["1", "1", "1", "1", "1", "1", "2", "2", "1", "1", "0.5", "1", "1", "1", "1"]},
-        {"key":"none","Bug": ["1", "0.5", "1", "1", "2", "1", "0.5", "2", "1", "0.5", "2", "1", "1", "0.5", "1"]},
-        {"key":"none","Rock": ["1", "2", "1", "1", "1", "2", "0.5", "1", "0.5", "2", "1", "2", "1", "1", "1"]},
-        {"key":"none", "Ghost": ["0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "1", "1", "2", "1"]}
+        {"key":"Flying","values": ["1", "1", "1", "0.5", "2", "1", "2", "1", "1", "1", "1", "2", "0.5", "1", "1"]},
+        {"key":"Psychic","values": ["1", "1", "1", "1", "1", "1", "2", "2", "1", "1", "0.5", "1", "1", "1", "1"]},
+        {"key":"Bug","values": ["1", "0.5", "1", "1", "2", "1", "0.5", "2", "1", "0.5", "2", "1", "1", "0.5", "1"]},
+        {"key":"Rock","values": ["1", "2", "1", "1", "1", "2", "0.5", "1", "0.5", "2", "1", "2", "1", "1", "1"]},
+        {"key":"Ghost", "values": ["0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "1", "1", "2", "1"]},
+        {"key":"Dragon", "values": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2"]}
+    ],
+
+    "Defense": [
+        {"key":"Normal", "values": ["1", "1", "1", "1", "1", "1", "2", "1", "1", "1", "1", "1", "1", "0", "1"]},
+        {"key":"Fire", "values": ["1", "0.5", "2", "1", "0.5", "1", "1", "1", "2", "1", "1", "0.5", "2", "1", "1"]},
+        {"key":"Water", "values": ["1", "0.5", "0.5", "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]},
+        {"key":"Electric","values": ["1", "1", "1", "0.5", "1", "1", "1", "1", "2", "0.5", "1", "1", "1", "1", "1"]},
+        {"key":"Grass","values": ["1", "2", "0.5", "0.5", "0.5", "2", "1", "2", "0.5", "2", "1", "2", "1", "1", "1"]},
+        {"key":"Ice","values": ["1", "2", "1", "1", "1", "0.5", "2", "1", "1", "1", "1", "1", "2", "1", "1"]},
+        {"key":"Fighting","values": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "0.5", "0.5", "1", "1"]},
+        {"key":"Poison","values": ["1", "1", "1", "1", "0.5", "1", "0.5", "0.5", "1", "1", "2", "2", "2", "1", "1"]},
+        {"key":"Ground", "values": ["1", "1", "2", "0", "2", "2", "1", "0.5", "1", "1", "1", "1", "0.5", "1", "1"]},
+        {"key":"Flying","values": ["1", "1", "1", "1", "1", "2", "0.5", "1", "0", "1", "1", "0.5", "2", "1", "1"]},
+        {"key":"Psychic","values": ["1", "1", "1", "1", "1", "1", "0.5", "1", "1", "1", "0.5", "2", "1", "0", "1"]},
+        {"key":"Bug","values": ["1", "2", "1", "1", "0.5", "1", "0.5", "2", "0.5", "2", "1", "1", "2", "1", "1"]},
+        {"key":"Rock","values": ["0.5", "0.5", "2", "1", "2", "1", "2", "0.5", "2", "0.5", "1", "1", "1", "1", "1"]},
+        {"key":"Ghost", "values": ["0", "1", "1", "1", "1", "1", "0", "0.5", "1", "1", "1", "0.5", "1", "2", "1"]},
+        {"key":"Dragon", "values": ["1", "0.5", "0.5", "0.5", "0.5", "2", "1", "1", "1", "1", "1", "1", "1", "1", "2"]}
     ]
 }
 
 function findDb(code) {
-    var dbList = [];
+    var attackList = [];
+    var defenseList = [];
+
     var supers = [];
     var half = [];
-    switch(code) {
-        case "Normal":
-            dbList = db.Attack[0].values;
-            break;
-        case "Fire":
-            dbList = db.Attack[1].values;
-            break;
-        case "Ground":
-            dbList = db.Attack[8].values;
-            break;
+    var weak = [];
+    var resist = [];
+    var noEffect = [];
+    var immune = [];
 
+    for(var i in db["Attack"]) {
+        if(db["Attack"][i].key == code)
+            attackList = db["Attack"][i].values;
     }
-    supers = findEffect(dbList, "2");
-    half = findEffect(dbList, "0.5");
-    noEffect = findEffect(dbList, "0");
+    for(var i in db["Defense"]) {
+        if(db["Defense"][i].key == code)
+            defenseList = db["Defense"][i].values;
+    }
 
-    document.getElementById("super-content-container").style.height = 'fit-content';
-    document.getElementById("half-content-container").style.height = 'fit-content';
+    supers = findEffect(attackList, "2");
+    half = findEffect(attackList, "0.5");
+    noEffect = findEffect(attackList, "0");
 
-    document.getElementById("effect-break").innerHTML = "";
-    document.getElementById("super-content-container").innerHTML = "";
-    document.getElementById("half-content-container").innerHTML = "";
-    document.getElementById("no-effect-content-container").innerHTML = "";
+    weak = findEffect(defenseList, "2");
+    resist = findEffect(defenseList, "0.5");
+    immune = findEffect(defenseList, "0");
+
+    resetHtml();
 
     document.getElementById("effect-break").innerHTML += "<br>";
-    document.getElementById("super-content-container").innerHTML += '<h5>Super Effective<h5>';
-    for(var i in supers) {
-        console.log(supers[i]);
-        document.getElementById("super-content-container").innerHTML+=
-        '<div id="' + supers[i] + '-text">' + supers[i] + '</div><br>';
-    }
-    document.getElementById("half-content-container").innerHTML += '<h5>Half Effective<h5>';
-    for(var i in half) {
-        console.log(half[i]);
-        document.getElementById("half-content-container").innerHTML+=
-        '<div id="' + half[i] + '-text">' + half[i] + '</div><br>';
+    if(supers.length > 0) {
+        document.getElementById("super-content-container").innerHTML += '<h6>Super Effective<h6>';
+        for(var i in supers) {
+            document.getElementById("super-content-container").innerHTML+=
+            '<div id="' + supers[i] + '-text">' + supers[i] + '</div><br>';
+        }
+    } else {
+        document.getElementById("half-content-container").style.width = "100%";
+        document.getElementById("super-content-container").style.width = "0%";
     }
 
+    if(half.length > 0) {
+        document.getElementById("half-content-container").innerHTML += '<h6>Half Effective<h6>';
+        for(var i in half) {
+            document.getElementById("half-content-container").innerHTML+=
+            '<div id="' + half[i] + '-text">' + half[i] + '</div><br>';
+        }
+    } else {
+        document.getElementById("half-content-container").style.width = "0%";
+        document.getElementById("super-content-container").style.width = "100%";
+    }
+    equalHeightsUpperContent();
+
+    if(resist.length > 0) {
+        document.getElementById("resist-content-container").innerHTML += "<br>";
+        document.getElementById("resist-content-container").innerHTML += '<h6>Resists<h6>';
+        for(var i in resist) {
+            document.getElementById("resist-content-container").innerHTML+=
+            '<div id="' + resist[i] + '-text">' + resist[i] + '</div><br>';
+        }
+    } else {
+        document.getElementById("resist-content-container").style.width = "0%";
+        document.getElementById("weak-content-container").style.width = "100%";
+    }
+
+    if(weak.length > 0) {
+        document.getElementById("weak-content-container").innerHTML += "<br>";
+        document.getElementById("weak-content-container").innerHTML += '<h6>Weak Against<h6>';
+        for(var i in weak) {
+            document.getElementById("weak-content-container").innerHTML+=
+            '<div id="' + weak[i] + '-text">' + weak[i] + '</div><br>';
+        }
+    } else {
+        document.getElementById("resist-content-container").style.width = "100%";
+        document.getElementById("weak-content-container").style.width = "0%";
+    }
+    adjustHeightMidContent();
+
     if(noEffect.length > 0) {
-        document.getElementById("no-effect-content-container").innerHTML += '<h5>No Effect<h5>';
+        document.getElementById("no-effect-content-container").innerHTML += '<br><h6>No Effect Against<h6>';
         for(var i in noEffect) {
             document.getElementById("no-effect-content-container").innerHTML+=
             '<div id="' + noEffect[i] + '-text">' + noEffect[i] + '</div><br>';
         }
+    } else {
+        document.getElementById("no-effect-content-container").style.height = "0%";
+        document.getElementById("immune-content-container").style.height = "100%";
     }
+    if(immune.length > 0) {
+        document.getElementById("immune-content-container").innerHTML += "<br>";
+        document.getElementById("immune-content-container").innerHTML += '<h6>Immune To<h6>';
+        for(var i in immune) {
+            document.getElementById("immune-content-container").innerHTML+=
+            '<div id="' + immune[i] + '-text">' + immune[i] + '</div><br>';
+        }
+    } else {
+        document.getElementById("no-effect-content-container").style.width = "100%";
+        document.getElementById("immune-content-container").style.width = "0%";
+    }
+    adjustHeightBottomContent();
 
-    equalHeightsUpperContent();
-    
+    document.getElementById("line-break").innerHTML += "<br>";
+    document.getElementById("body-wrapper").style.backgroundColor = "#999999";
+
+    document.getElementById("effect-content-upper-wrapper").style.height = document.getElementById("upper-content-wrapper").clientHeight;
+    document.getElementById("effect-content-middle-wrapper").style.height = document.getElementById("middle-content-wrapper").clientHeight;
+    document.getElementById("effect-content-bottom-wrapper").style.height = document.getElementById("bottom-content-wrapper").clientHeight;
 }
-
-
 
 function findEffect(list, damage) {
     var supers = [];
@@ -89,13 +162,104 @@ function equalHeightsUpperContent() {
 
     superHeight = document.getElementById("super-content-container").clientHeight;
     halfHeight = document.getElementById("half-content-container").clientHeight;
-    console.log('super: '+superHeight+' half: '+halfHeight)
+
     if(superHeight != halfHeight) {
-        if(superHeight > halfHeight)
+        if(superHeight > halfHeight) {
             document.getElementById("half-content-container").style.height = superHeight;
-        else 
+            document.getElementById("effect-content-upper-wrapper").style.height = superHeight;
+            document.getElementById("upper-content-wrapper").style.height = superHeight;
+        }
+        else {
             document.getElementById("super-content-container").style.height = halfHeight;
+            document.getElementById("effect-content-upper-wrapper").style.height = halfHeight;
+            document.getElementById("upper-content-wrapper").style.height = halfHeight;
+        }
     }   
+    else {
+        document.getElementById("half-content-container").style.height = superHeight;
+        document.getElementById("effect-content-upper-wrapper").style.height = superHeight;
+        document.getElementById("upper-content-wrapper").style.height = superHeight;
+    }
+}
+
+function adjustHeightMidContent() {
+    var resistHeight = 0;
+    var weakHeight = 0;
+
+    resistHeight = document.getElementById("resist-content-container").clientHeight;
+    weakHeight = document.getElementById("weak-content-container").clientHeight;
+
+    if(resistHeight != weakHeight) {
+        if(resistHeight > weakHeight) {
+            document.getElementById("weak-content-container").style.height = resistHeight;
+            document.getElementById("effect-content-middle-wrapper").style.height = resistHeight;
+            document.getElementById("middle-content-wrapper").style.height = resistHeight;
+        }
+        else {
+            document.getElementById("resist-content-container").style.height = weakHeight;
+            document.getElementById("effect-content-middle-wrapper").style.height = weakHeight;
+            document.getElementById("middle-content-wrapper").style.height = weakHeight;
+        }
+    }   
+    else {
+        document.getElementById("weak-content-container").style.height = resistHeight;
+        document.getElementById("effect-content-middle-wrapper").style.height = resistHeight;
+        document.getElementById("middle-content-wrapper").style.height = resistHeight;
+    }
+}
+
+function adjustHeightBottomContent() {
+    var resistHeight = 0;
+    var weakHeight = 0;
+
+    resistHeight = document.getElementById("no-effect-content-container").clientHeight;
+    weakHeight = document.getElementById("immune-content-container").clientHeight;
+
+    if(resistHeight != weakHeight) {
+        if(resistHeight > weakHeight) {
+            document.getElementById("immune-content-container").style.height = resistHeight;
+            document.getElementById("effect-content-bottom-wrapper").style.height = resistHeight;
+            document.getElementById("bottom-content-wrapper").style.height = resistHeight;
+        }
+        else {
+            document.getElementById("no-effect-content-container").style.height = weakHeight;
+            document.getElementById("effect-content-bottom-wrapper").style.height = weakHeight;
+            document.getElementById("bottom-content-wrapper").style.height = weakHeight;
+        }
+    }   
+    else {
+        document.getElementById("immune-content-container").style.height = resistHeight;
+        document.getElementById("effect-content-bottom-wrapper").style.height = resistHeight;
+        document.getElementById("bottom-content-wrapper").style.height = resistHeight;
+    }
+}
+
+function resetHtml() {
+    document.getElementById("half-content-container").style.width = "50%";
+    document.getElementById("super-content-container").style.width = "50%";
+    document.getElementById("resist-content-container").style.width = "50%";
+    document.getElementById("weak-content-container").style.width = "50%";
+    document.getElementById("no-effect-content-container").style.width = "50%";
+    document.getElementById("immune-content-container").style.width = "50%";
+
+
+    document.getElementById("super-content-container").style.height = 'fit-content';
+    document.getElementById("half-content-container").style.height = 'fit-content';
+    document.getElementById("effect-content-bottom-wrapper").style.height = 'fit-content';
+    document.getElementById("resist-content-container").style.height = 'fit-content';
+    document.getElementById("weak-content-container").style.height = 'fit-content';
+    document.getElementById("no-effect-content-container").style.height = "fit-content";
+    document.getElementById("immune-content-container").style.height = "fit-content";
+
+    document.getElementById("effect-break").innerHTML = "";
+    document.getElementById("super-content-container").innerHTML = "";
+    document.getElementById("half-content-container").innerHTML = "";
+    document.getElementById("no-effect-content-container").innerHTML = "";
+    document.getElementById("resist-content-container").innerHTML = "";
+    document.getElementById("weak-content-container").innerHTML = "";
+    document.getElementById("no-effect-content-container").innerHTML = "";
+    document.getElementById("immune-content-container").innerHTML = "";
+
 }
 
 function numberToString(key) {
